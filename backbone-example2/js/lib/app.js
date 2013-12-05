28da33a5f68c49c36349a053773c9182a840caa7
JFingert <joshuafingert@gmail.com>
2013-12-05 15:33:39 -0800

    done for now

--- backbone-example2/js/lib/app.js
+++ backbone-example2/js/lib/app.js
@@ -4,33 +4,20 @@ $(function () { // wait for on-ready
 	//and "little number when below 100"
 
 	var AppView = Backbone.View.extend({
-		el: '#random-number-app', '#random-number-comentary',//every backbone view has an associated dom element
-
-		events: {
-			'click button': 'randomButtonPressed'
-		},
+		el: '#random-number-app',//every backbone view has an associated dom element
 
 		initialize: function () {
 			this.render();
 		},
 
 		template: function (context) {
-			return "<h1>" + context.rannum + "</h1>" + 
-			'<button class="pure-button pure-button-primary">Random</button>';
+			return "<h1>" + context.rannum + "</h1>";
 		},
 
 		render: function () {
 			this.$el.html(this.template(this.model.attributes));
 			return this;
-		},
-
-		randomButtonPressed: function () {
-			this.model.newRandomNumber();
 		}
-
-		// comment: function () {
-		// 	this.model.commentary();
-		// }
 	});
 
 	var AppModel = Backbone.Model.extend({
@@ -43,30 +30,53 @@ $(function () { // wait for on-ready
 			var number = Math.floor(Math.random() * 1000);
 			this.set('rannum', number);
 
-		},
-
-		commentary: function () {
-			if (this.newRandomNumber.number > 900) {
-				return "<h1>Whoa! That's a HUGE number, baby!!!</h1>";
-			} else if (this.newRandomNumber.number < 100){
-				return "<h1>Yikes! That's a teeny-tiny number, lil guy!!!</h1>";
-			} 
 		}
 	});
 
-	// var AppCommentary = Backbone.Model.extend({
+	var AppCommentary = Backbone.View.extend({
+		el: '#random-number-comentary',//every backbone view has an associated dom element
 
+		events: {
+			'click button': 'randomButtonPressed'
+		},
+
+		initialize: function () {
+			this.render();
+		},
+
+		template: function (str) {
+			return '<button class="pure-button pure-button-primary">Random</button>' +
+			'<h2>' + str + '</h2>';
+		},
+
+		render: function () {
+			var randomNumber = this.model.get('rannum');
+			if (randomNumber > 900) {
+				 this.$el.html(this.template("Whoa! That's a HUGE number, baby!!!"));
+			} else if (randomNumber < 100){
+				 this.$el.html(this.template("Yikes! That's a teeny-tiny number, lil guy!!!"));
+			} else {
+				this.$el.html(this.template("I absolutely love numbers!"));
+			}
+		},
+
+		randomButtonPressed: function () {
+			this.model.newRandomNumber();
+		}
 		
-	// });
+	});
 
 var myModel = new AppModel();
 
 var app = new AppView({model: myModel});
 
-	//var commentary = new AppView({model: myModel});
+var commentary = new AppCommentary({model: myModel});
 
-	app.listenTo(myModel, 'change', app.render);
+app.listenTo(myModel, 'change', app.render);
 
-	window.app = app;
+commentary.listenTo(myModel, 'change', commentary.render);
+
+
+window.app = app;
 
 });
